21f968f0507b8825197c4fdfc448fdbf84ad30e7
nandiphandlovu <biameleng@icloud.com>
2023-08-30 14:21:42 +0200

    succes in getting the token

--- codehesion-app/src/App.jsx
+++ codehesion-app/src/App.jsx
@@ -2,12 +2,15 @@
 
 import './App.css'
 
-import {Login} from './componets/Login';
+import { Login } from './componets/Login';
+import { Register } from './componets/Register';
 
 function App() {
   return (
     <>
-      <Login/>
+      <Login />
+      <Register/>
+
     </>
   )
 }
