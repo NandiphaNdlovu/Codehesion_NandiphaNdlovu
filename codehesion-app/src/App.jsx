fe3f2b795c07ecd25790a2f93fc00f20f007e57a
NandiphaNdlovu <biameleng@icloud.com>
2023-08-31 23:27:17 +0200

    added routes

--- codehesion-app/src/App.jsx
+++ codehesion-app/src/App.jsx
@@ -2,19 +2,27 @@
 
 import './App.css'
 
+import Home from './componets/Home'
 import { Login } from './componets/Login';
 import { Register } from './componets/Register';
 
+// routes
+import {Routes, Route} from 'react-router-dom'
 function App() {
   return (
     <>
+    {/*
     <h1>Home Page</h1>
     <div>
       <Login />
       <Register/>
     </div>
-      
-      
+      */}
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/register' element={<Register/>}/>
+      </Routes>
     </>
   )
 }
--- codehesion-app/src/componets/Login.jsx
+++ codehesion-app/src/componets/Login.jsx
@@ -24,11 +24,12 @@ export const Login = () => {
     method: 'post',
     maxBodyLength: Infinity,
     url: 'https://edeaf-api-staging.azurewebsites.net/connect/token',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
+    headers: { 
+      'Content-Type': 'application/x-www-form-urlencoded', 
+      'Authorization': 'eyJhbGciOiJSUzI1NiIsImtpZCI6IjRDRDlGNDg5RDEwOTg4QjA5NDk3RjVBM0Y0OTc4M0Q4MUFEMkI2MDBSUzI1NiIsInR5cCI6ImF0K2p3dCIsIng1dCI6IlRObjBpZEVKaUxDVWxfV2o5SmVEMkJyU3RnQSJ9.eyJuYmYiOjE2OTM0NjIxNzcsImV4cCI6MTY5MzQ2NTc3NywiaXNzIjoiaHR0cHM6Ly9lZGVhZi1hcGktc3RhZ2luZy5henVyZXdlYnNpdGVzLm5ldCIsImF1ZCI6ImVEZWFmQXBpIiwiY2xpZW50X2lkIjoid2ViLWRhc2hib2FyZCIsInN1YiI6IjFiY2QzMWYyLWIxZTgtNGU3ZS1hMTA4LTY0ZjgyZTFkMzMwYSIsImF1dGhfdGltZSI6MTY5MzQ2MjE3NywiaWRwIjoibG9jYWwiLCJyb2xlIjoiQWRtaW5pc3RyYXRvciIsImp0aSI6IkFFRkUzMjgwMjg1MzcxQ0RCNjAwRjQ4RTNCRUMzN0Y5IiwiaWF0IjoxNjkzNDYyMTc3LCJzY29wZSI6WyJhZG1pbkFwaSIsImVtYWlsIiwibW9iaWxlQXBpIiwib3BlbmlkIiwicHJvZmlsZSIsInJvbGUiLCJvZmZsaW5lX2FjY2VzcyJdLCJhbXIiOlsicHdkIl19.cnzPKtpi-gaY7faHAQ_2xQd-xh6pf-bpz51l46i1DC_MhasH33HowRN3qMs8dBC7fkZzLe9KBWdyjcAgAxQVxngGi0DxuU5vgIxCR9ehFLSYlYDXA-YkfT9u9r5XEF_DpkQF1NP4UP2yae3oGSO4-GHHr7XTp7qTchaUwq_U4IXIxRepBsLX-c-kkCyMdQjai01JZqhxT8TSrxzAkl_HBIgotHDlJzb3N_QDppkmvcYHR2cemm4Hv1ZGnIySaYNvO1by5OINAQ1SyNpALlx0c1GGuYWIz7vd5Eirog3Wky3mGHKdr-khiqPiHXxlYWACmWTkl9Eg9IK2SiDcXdrF6g', 
       'Cookie': 'ARRAffinity=c03b4673f6f23ebd4ee8b0530ffb43a9bf80e737379a4092ddb472e0648d0a07; ARRAffinitySameSite=c03b4673f6f23ebd4ee8b0530ffb43a9bf80e737379a4092ddb472e0648d0a07'
     },
-    data: data
+    data : data
   };
   //trying useState and use effect
   useEffect(()=>{
@@ -39,7 +40,7 @@ export const Login = () => {
     //do login validation here then part means that correct 
     //local storage check if something correct 
 
-    //do error handlink 
+    //do error  
     axios.request(config)
       .then((response) => {
         setToken_o(response.data.access_token)
@@ -59,7 +60,7 @@ export const Login = () => {
             //call axios 
             //shouldnt be here must b in separate use the layers to make it look good 
             if (values.uname === data.username && values.pword === data.password) {
-              console.log(values,"correct",token_o)
+              console.log(values,"correct")
               //ehat does this code do?
               login()
               await new Promise((resolve) => setTimeout(resolve, 5000));
